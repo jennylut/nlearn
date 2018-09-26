c19253916312afd646584f83d89c19f33200a35f
2865724220 <2865724220@qq.com>
2018-09-26 14:16:45 +0800

    page add

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express()
-app.get('/'function(req,res){
-	res.send('hello world')
+var bodyParser = require('body-parser');
+ 
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+app.use(express.static('public'));
+app.get('/index.html',function(req,res){
+	// res.send('hello world')
+	res.sendFile(__dirname + '/' +'index.html')
 });
-app.listen(4000,function(){
+
+app.post('/',function(req,res){
+	console.log(' Hello post')
+	res.send('post')
+});
+app.get('/list',function(req,res){
+	res.send('列表页面')
+});
+var server = app.listen(4000,function(){
+	var host = server.address().address
+	var port = server.address().port
+	console.log(host,port)
 	console.log('app is listening at port 4000')
 })
