cdf32b6408b1d47b14303046357f39d1aac0168a
2865724220 <2865724220@qq.com>
2018-09-27 11:51:10 +0800

    用superagent 和 cheerio爬网站数据

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var utility = require('utility');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+
 var app = express()
 var bodyParser = require('body-parser');
  
@@ -6,10 +10,33 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 app.use(express.static('public'));
-app.get('/index.html',function(req,res){
-	// res.send('hello world')
-	res.sendFile(__dirname + '/' +'index.html')
-});
+// app.get('/index.html',function(req,res){
+// 	// res.send('hello world')
+// 	res.sendFile(__dirname + '/' +'index.html')
+// });
+// app.get('/',function(req,res){
+// 	var q = req.query.q
+// 	var md5Value = utility.md5(q)
+// 	res.send(md5Value)
+// })
+app.get('/',function(req,res){
+	superagent.get('https://cnodejs.org/')
+	.end(function(err,sres){
+		if(err){
+			return next(error)
+		}
+		var $ = cheerio.load(sres.text);
+		var items =[];
+		$('#topic_list .topic_title').each(function(index,element){
+			var $element = $(element)
+			items.push({
+				title:$element.attr('title'),
+				href:$element.attr('href')
+			});
+		});
+		res.send(items)
+	})
+})
 
 app.post('/',function(req,res){
 	console.log(' Hello post')
@@ -21,6 +48,6 @@ app.get('/list',function(req,res){
 var server = app.listen(4000,function(){
 	var host = server.address().address
 	var port = server.address().port
-	console.log(host,port)
-	console.log('app is listening at port 4000')
+	console.log('运行环境端口'+host,port)
+	console.log('app port 4000')
 })
