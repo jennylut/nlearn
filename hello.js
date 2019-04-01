83fc328768565c8ffe31e11eaf014eb05e4f839a
Jenny <jennygao@JennydeMacBook-Pro.local>
2019-04-01 18:01:32 +0800

    实现本地图片上传

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+var express =require('express')
+var app = express()
+
+// app.get('/',function(req,res){
+//     res.send('hello node')
+// })
+// app.use(express.static('public'))
+app.use(express.static('views'))
+app.get('views/start.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "start.html" );
+ })
+
+var server = app.listen(8001,function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('demo listening at',host,port)
+})
---
+++ server/fileUpload.js
@@ -0,0 +1,34 @@
+var express =require('express')
+var formiadable = require('formidable')
+var app = express()
+var fs = require('fs')
+
+var timestamp = new Date().getTime()
+
+app.use(express.static('views'))
+//get的目录是相对于根目录的，不是相对于fileUpload.js这个文件的位置
+app.get('views/start.html', function (req, res) {
+    console.log(__dirname)
+    res.sendFile( __dirname + "/" + "start.html" );
+ })
+
+app.post('/upload',function(req,res){
+    var form = new formiadable.IncomingForm()
+    console.log(form)
+    form.parse(req,function(error,fields,files){
+        console.log('parsing done')
+        console.log(files.upload.path)
+        fs.writeFileSync(`common/imgs/${timestamp}.jpg`, fs.readFileSync(files.upload.path))
+        // res.wirte('上传成功！')
+        setTimeout(()=>{
+            res.redirect('upload.html')
+        },100)
+    })
+})
+
+var server = app.listen(8001,function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('demo listening at',host,port)
+})
